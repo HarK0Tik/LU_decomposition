c8e8c24f7f532c28fc9dfb86a13d16b6057264fe
Sergeev Sergey <sereojasergeev@gmail.com>
2023-06-15 13:20:22 +0300

    cheking the correctness of the answer

--- main.cpp
+++ main.cpp
@@ -12,11 +12,12 @@ int main()
 	int n;
 	cin >> n;
 	cout << "Введите матрицу: " << endl;
-	double** m = new double*[n], **l = new double* [n], **u = new double* [n];
+	double** m = new double* [n], ** l = new double* [n], ** u = new double* [n], ** p = new double* [n];
 	for (int i = 0; i < n; ++i) {
 		m[i] = new double[n];
 		l[i] = new double[n];
 		u[i] = new double[n];
+		p[i] = new double[n];
 		for (int j = 0; j < n; ++j) {
 			cin >> m[i][j];
 			u[i][j] = m[i][j];
@@ -28,10 +29,10 @@ int main()
 	for (int i = 1; i < n; ++i) {
 		for (int j = i; j < n; ++j)
 		{
-			l[j][i-1] = u[j][i-1] / u[i-1][i-1];
+			l[j][i - 1] = u[j][i - 1] / u[i - 1][i - 1];
 			for (int k = 0; k < n; ++k) {
 
-				u[j][k] -= u[i-1][k] * l[j][i-1];
+				u[j][k] -= u[i - 1][k] * l[j][i - 1];
 			}
 		}
 	}
@@ -39,21 +40,36 @@ int main()
 	cout << "Исходная матрица:" << endl;
 	for (int i = 0; i < n; ++i) {
 		for (int j = 0; j < n; ++j)
-			cout << setw(8) << m[i][j] << ' ';
+			cout << setw(10) << m[i][j] << ' ';
 		cout << endl;
 	}
 
 	cout << "Матрица U:" << endl;
 	for (int i = 0; i < n; ++i) {
 		for (int j = 0; j < n; ++j)
-			cout << setw(8) << u[i][j] << ' ';
+			cout << setw(10) << u[i][j] << ' ';
 		cout << endl;
 	}
 
 	cout << "Матрица L:" << endl;
 	for (int i = 0; i < n; ++i) {
 		for (int j = 0; j < n; ++j)
-			cout << setw(8) << l[i][j] << ' ';
+			cout << setw(10) << l[i][j] << ' ';
+		cout << endl;
+	}
+
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < n; ++j) {
+			p[i][j] = 0;
+			for (int k = 0; k < n; ++k)
+				p[i][j] += l[i][k] * u[k][j];
+		}
+	}
+
+	cout << "Проверка:" << endl;
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < n; ++j)
+			cout << setw(10) << p[i][j] << ' ';
 		cout << endl;
 	}
 }
